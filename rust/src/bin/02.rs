bf75d4097e56bf70bf53f1ef4524eefa2819e91d
Walther Chen <walther.chen@gmail.com>
2019-12-04 17:43:41 -0500

    move intcode computer to lib

--- rust/src/bin/02.rs
+++ rust/src/bin/02.rs
@@ -1,3 +1,5 @@
+use advent_2019::run_intcode;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::fs::read_to_string("../input/02.txt")?;
 
@@ -11,47 +13,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     program_1[1] = 12;
     program_1[2] = 2;
 
-    let program_1 = run_1(program_1);
+    let program_1 = run_intcode(program_1);
 
     println!("Answer 1: {}", program_1[0]);
 
     Ok(())
 }
 
-fn run_1(program: Vec<u64>) -> Vec<u64> {
-    let mut program = program;
-    let mut cursor = 0;
-
-    while let Some(opcode) = program.get(cursor) {
-        match opcode {
-            1 => {
-                let register = program[cursor + 3] as usize;
-                program[register] = program[program[cursor + 1] as usize] + program[program[cursor + 2] as usize];
-                cursor += 4;
-            },
-            2 => {
-                let register = program[cursor + 3] as usize;
-                program[register] = program[program[cursor + 1] as usize] * program[program[cursor + 2] as usize];
-                cursor += 4;
-            },
-            99 => {
-                break;
-            },
-            _ => panic!("logic error"),
-        }
-    }
-
-    program
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_run_1() {
-        assert_eq!(run_1(vec![1,0,0,0,99]), vec![2,0,0,0,99]);
-        assert_eq!(run_1(vec![2,3,0,3,99]), vec![2,3,0,6,99]);
-        assert_eq!(run_1(vec![1,1,1,4,99,5,6,0,99]), vec![30,1,1,4,2,5,6,0,99]);
-    }
-}
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,37 @@
+pub fn run_intcode(program: Vec<u64>) -> Vec<u64> {
+    let mut program = program;
+    let mut cursor = 0;
+
+    while let Some(opcode) = program.get(cursor) {
+        match opcode {
+            1 => {
+                let register = program[cursor + 3] as usize;
+                program[register] = program[program[cursor + 1] as usize] + program[program[cursor + 2] as usize];
+                cursor += 4;
+            },
+            2 => {
+                let register = program[cursor + 3] as usize;
+                program[register] = program[program[cursor + 1] as usize] * program[program[cursor + 2] as usize];
+                cursor += 4;
+            },
+            99 => {
+                break;
+            },
+            _ => panic!("logic error"),
+        }
+    }
+
+    program
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_run_intcode() {
+        assert_eq!(run_1(vec![1,0,0,0,99]), vec![2,0,0,0,99]);
+        assert_eq!(run_1(vec![2,3,0,3,99]), vec![2,3,0,6,99]);
+        assert_eq!(run_1(vec![1,1,1,4,99,5,6,0,99]), vec![30,1,1,4,2,5,6,0,99]);
     }
 }
