557220e9058cf52298d6c0564f538b6a563e6f37
Walther Chen <walther.chen@gmail.com>
2019-12-04 19:05:38 -0500

    [rust] 2 part 2

--- rust/src/bin/02.rs
+++ rust/src/bin/02.rs
@@ -15,7 +15,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Answer 1: {}", res_1);
 
     // part 2, search for program inputs that result in in output 19690720
-    
+    let mut res = None;
+
+    // Inefficient, but sufficient
+    for i in 0..=99 {
+        for j in 0..=99 {
+            let output = intcode.run(i,j);
+            if output == 19690720 {
+                res = Some((i,j));
+            }
+        }
+    }
+
+    let res = res.unwrap();
+    println!("Answer 2: {}", 100 * res.0 + res.1);
 
     Ok(())
 }
